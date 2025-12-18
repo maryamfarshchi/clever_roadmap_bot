6af67fb6659c2939863bf73572cbf5989faab3de
maryamfarshchi <M.FARSHCHI20@GMAIL.COM>
2025-12-18 17:10:24 +0330

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -5,4 +5,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GOOGLE_API_URL = os.getenv("GOOGLE_API_URL").rstrip("/") if os.getenv("GOOGLE_API_URL") else ""
-CACHE_TTL = 300  # زمان انقضای کش در ثانیه
+CACHE_TTL = int(os.getenv("CACHE_TTL", "60"))
